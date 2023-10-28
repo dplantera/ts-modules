3fa08c2d2a03b6c58bbfe4a1140ff806f29c1791
danie <daniele.plantera@outlook.de>
2023-10-28 13:04:52 +0200

    feat(svg): axis offset

--- packages/svg/index.ts
+++ packages/svg/index.ts
@@ -7,7 +7,7 @@ async function main() {
     const seriesA = [{x: 0, y: 10}, {x: 5, y: 5}];
     const seriesB = [{x: 5.1, y: 4.9}, {x: 10, y: 0}];
     const chart = await SvgChart.fromFile("template.svg", {
-        axis: {id: {y: "y-axis", x: "x-axis"}},
+        axis: {id: {y: "y-axis", x: "x-axis"}, offset: {x: 0.1, y: 0.4}},
         graphs: {filledLines: [{data: seriesA, id: "tilgung"}, {data: seriesB, id: 'anfi'}]},
         pointsOfInterest: ["poi-1"]
     });
