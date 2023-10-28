a5808247b33b7497511ee9022666b34112daba65
danie <daniele.plantera@outlook.de>
2023-10-28 21:13:53 +0200

    fix(svg): test

--- packages/svg/src/svg.test.ts
+++ packages/svg/src/svg.test.ts
@@ -1,12 +1,18 @@
-import {findPointAboveLine} from "./svgChart";
+import {findPointAboveLine} from "./svg-chart";
 
 describe("svg", () => {
     test("findPointAboveLine", () => {
-        const p1 = () => ({x: 0, y: 0});
-        const p2 = () => ({x: 5, y:5});
-        expect(findPointAboveLine(1, p1(), p2())).toEqual({x: 1, y: 1})
-        expect(findPointAboveLine(3, p1(), p2())).toEqual({x: 3, y: 3})
-        expect(findPointAboveLine(0, p1(), p2())).toEqual({x: 0, y: 0})
-        expect(findPointAboveLine(5, p1(), p2())).toEqual({x: 5, y: 5})
+        /*
+          (0,0)|
+               |  o (x, y_max - y)
+               |
+     (0, y_max) - - - - - - (x_max, y_max)
+         */
+        const yMax = 5, xMax = 5;
+        const p1 = () => ({x: 0, y: 0}), p2 = () => ({x: xMax, y: yMax}) ;
+        expect(findPointAboveLine(1, p1(), p2())).toEqual({x: 1, y: yMax - 1})
+        expect(findPointAboveLine(3, p1(), p2())).toEqual({x: 3, y: yMax - 3})
+        expect(findPointAboveLine(0, p1(), p2())).toEqual({x: 0, y: yMax})
+        expect(findPointAboveLine(5, p1(), p2())).toEqual({x: 5, y: yMax - 5})
     })
 })
