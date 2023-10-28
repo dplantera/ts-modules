3febf476818d6143b3df9ee7d5fcb92a53305a99
danie <daniele.plantera@outlook.de>
2023-10-28 12:54:33 +0200

    feat(svg): draw graphs

--- packages/svg/src/svgChart.ts
+++ packages/svg/src/svgChart.ts
@@ -1,8 +1,9 @@
 import {Svg, Vec2} from "./svg";
-import {INode} from "svgson";
 
 export interface ChartOptions {
-    axis: { id: { x: string, y: string } }
+    axis: { id: { x: string, y: string } },
+    graphs: { filledLines: Array<{ data: Array<Vec2>, id: string }> },
+    pointsOfInterest?: {}
 }
 
 export module SvgChart {
@@ -13,6 +14,13 @@ export module SvgChart {
     export function of(svg: Svg, opts: ChartOptions) {
         const yAxis = svg.mutSelectById(opts.axis.id.y);
         const xAxis = svg.mutSelectById(opts.axis.id.x);
+        const translatePoint = {
+            "default": (a: Vec2) => a
+        };
+        // we need a bucket with all points - so we can scale with alignment
+        const graphs = {
+            filledLines: opts.graphs.filledLines.flatMap(v => v.data.flatMap(d => ({...d, ref: v.id})))
+        };
         return {
             height() {
                 return yAxis.length().y
@@ -26,19 +34,30 @@ export module SvgChart {
             save(filePath: string) {
                 return svg.save(filePath);
             },
-            translate(data: Array<Vec2>) {
-                const xData = data.map(d => d.x);
-                const yData = data.map(d => d.y);
-                const xDataMin = Math.min(...xData);
-                const xDataMax = Math.max(...xData);
-                const yDataMin = Math.min(...yData);
-                const yDataMax = Math.max(...yData);
+            extremePoints() {
+                // fixme: maybe optimize
+                const xData = graphs.filledLines.map(d => d.x)
+                const yData = graphs.filledLines.map(d => d.y)
+                return {
+                    xDataMin: Math.min(...xData),
+                    xDataMax: Math.max(...xData),
+                    yDataMin: Math.min(...yData),
+                    yDataMax: Math.max(...yData),
+                }
+            },
 
+            translate(data: Array<Vec2>) {
+                const {xDataMin, yDataMin, yDataMax, xDataMax} = this.extremePoints();
                 const flipY = (p: number) => this.height() - p;
-                return data.map(d => ({
-                    x: scaleChart(d.x, xDataMax, xDataMin, this.width()),
-                    y: flipY(scaleChart(d.y, yDataMax, yDataMin, this.height()))
-                }))
+                // we may change it when we need multiple translation e.g. multiple y-axis
+                translatePoint.default = (vec: Vec2) => ({
+                    x: scaleChart(vec.x, xDataMax, xDataMin, this.width()),
+                    y: flipY(scaleChart(vec.y, yDataMax, yDataMin, this.height()))
+                })
+                return data.map(translatePoint.default)
+            },
+            translatePoint(vec: Vec2) {
+                return translatePoint.default(vec)
             },
             /*
               x1
@@ -46,14 +65,20 @@ export module SvgChart {
 
               x2           x4
              */
-            updateFilledLine(id: string, data: Array<Vec2>) {
+            updatePosFilledLine(id: string, data: Array<Vec2>) {
                 const pts = this.translate(data);
+                const first = pts[0];
                 const last = pts.slice(-1)[0];
-                const g1Tilgung = svg.mutSelectById(id);
-                const g2 = g1Tilgung.mutGet();
-                g2.attributes.d = `M ${pts[0].x} ${pts[0].y} ${pts.slice(1).map(s => (`L ${s.x} ${s.y}`))} L ${last.x} ${this.height()} L ${0} ${this.height()} Z`
 
-            }
+                svg.mutSelectById(id).mutGet().attributes.d = `M ${pts[0].x} ${pts[0].y} ${pts.slice(1).map(s => (`L ${s.x} ${s.y}`))} L ${last.x} ${this.height()} L ${first.x} ${this.height()} Z`
+            },
+            updatePosElement(id: string, pos: Vec2) {
+                const poi1 = svg.mutSelectById(id)
+                poi1.mutMove(this.translatePoint(pos))
+            },
+            updateFilledLineGraphs() {
+                opts.graphs.filledLines.forEach(({data, id}) => this.updatePosFilledLine(id, data))
+            },
         }
     }
 }
