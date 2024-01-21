346c447583ad559f27a298aac3a7b481088c683d
danie <daniele.plantera@outlook.de>
2024-01-21 12:54:07 +0100

    chore: added tests

--- packages/openapi/src/generate.test.ts
+++ packages/openapi/src/generate.test.ts
@@ -2,13 +2,6 @@ import { Folder } from "@dsp/node-sdk";
 import { generateOpenapi } from "./index.js";
 import path from "path";
 
-test("ggenerate openapi integration", async () => {
-  // const bundled = await generateOpenapi("test/specs/pets-modular/pets-api.yml", "test/out", { clearTemp: false });
-  const bundled = await generateOpenapi("test/specs/generic/api.yml", "test/out", { clearTemp: false });
-  const files = Folder.of(bundled).readAllFilesAsString();
-  files.forEach((f) => expect(f.content).toMatchSnapshot(`generate-openapi-${f.src}`));
-});
-
 describe("Generate Integration", () => {
   test.each([
     "test/specs/pets-modular/pets-api.yml",
@@ -16,7 +9,7 @@ describe("Generate Integration", () => {
     "test/specs/pets-modular-complex/petstore-api.yml",
     "test/specs/generic/api.yml",
   ])("%s", async (api) => {
-    const out = Folder.resolve("test/out", path.dirname(api)).absolutePath;
+    const out = Folder.resolve("test/out/integration", path.dirname(api)).absolutePath;
     const bundled = await generateOpenapi(api, out, { clearTemp: false });
     const files = Folder.of(bundled).readAllFilesAsString();
     files.forEach((f) => expect(f.content).toMatchSnapshot(`generate-openapi-${f.src}`));
